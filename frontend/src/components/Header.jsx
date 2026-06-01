e7e6eef4f424e700d392710d4bff89be4cb6bc05
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-06-01 19:32:29 -0400

    first commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router-dom"
+export default function Header() {
+    return (
+        <div className="bg-slate-200">
+            <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+                <Link to='/'>
+                    <h1 className="font-bold">auth app</h1>
+                </Link>
+                <ul className="flex gap-4">
+                    <Link to={'/'}>
+                        <li>home</li>
+                    </Link>
+                    <Link to={'/about'}>
+                        <li>about</li>
+                    </Link>
+                    <Link to={'/sign-in'}>
+                        <li>sign in</li>
+                    </Link>
+                </ul>
+            </div>
+        </div>
+    )
+}
