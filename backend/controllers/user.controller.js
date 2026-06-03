cb4070283792c2ed0060d98ebac050eaafc9265a
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-06-03 16:13:19 -0400

    first commit

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,45 @@
+import User from "../models/user.model.js"
+import { errorHandler } from "../utils/error.js"
+import bcryptjs from "bcryptjs"
 export const test = (req, res) => {
 
     res.json({ message: 'mern' })
+}
+export const updateUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) {
+        // return res.status(401).json('you can only update')
+        return next(errorHandler(401, 'update own account'))
+    }
+    try {
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10)
+        }
+        const updatedUser = await User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                    profilePicture: req.body.profilePicture
+                }
+            },
+            { new: true }
+        )
+        const { password, ...rest } = updatedUser._doc
+        res.status(200).json(rest)
+    } catch (error) {
+        next(error)
+    }
+}
+export const deleteUser = async (req, res, next) => {
+    if (req.user.id !== req.params.id) {
+        return next(errorHandler(401, 'you can delete only one account'))
+    }
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json('user has been deleted')
+    } catch (error) {
+        next(error)
+    }
 }
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,12 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
+import path from 'path'
+
+
+
 dotenv.config()
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -9,9 +15,25 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err) => {
     console.log(err)
 })
+const __dirname = path.resolve()
 const app = express()
-
+app.use(express.json())
+app.use(cookieParser())
 app.listen(3000, () => {
     console.log('server listening')
 })
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
+app.get('*splat', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'internal server error'
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    })
+})
